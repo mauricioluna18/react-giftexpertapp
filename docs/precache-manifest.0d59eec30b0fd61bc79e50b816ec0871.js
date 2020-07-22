54b90fae7c4ee2bf2c19b266e27524cd58a19f1f
Mauricio Luna <breack12@gmail.com>
2020-07-21 21:34:50 -0500

    docs agregado

---
+++ docs/precache-manifest.0d59eec30b0fd61bc79e50b816ec0871.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3ea13f75589f6eca7be94a3b542238b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b73ffafcba73403b83c1",
+    "url": "/static/css/main.78a8b6e8.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b73ffafcba73403b83c1",
+    "url": "/static/js/main.dd74e6d3.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
